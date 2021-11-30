cad811e655b196fecd6510fb14924e8747391b7a
henrique.catunda <henriquecatunda2@gmail.com>
2021-11-30 16:10:42 -0300

    refactor: atualização InputSearch

--- src/components/InputSearch/styles.tsx
+++ src/components/InputSearch/styles.tsx
@@ -7,16 +7,15 @@ export const Container = styled.div`
     justify-content: space-end;
     align-items: center;
     box-sizing: border-box;
-    border-bottom: 2px solid #BBB8D9;
     margin: auto;
     margin-top: 15px;
 `;
 
-export const Title = styled.div`
+export const Box = styled.div`
     margin: 14px 0 16px 18px;
     position: absolute;
 `;
 
-export const Box = styled.div`
-    color: ${props => props.theme.colors.subTilte};
+export const Title = styled.div`
+  color: ${props => props.theme.colors.subTilte};
 `;
