731a7bc43e18fad801dbf670447c8ba3e7aafb2d
henrique.catunda <henriquecatunda2@gmail.com>
2021-11-30 12:36:02 -0300

    feat:repositório Styles global

--- src/styles/global.tsx
+++ src/styles/global.tsx
@@ -11,4 +11,27 @@ export default createGlobalStyle`
     color: ${props => props.theme.colors.text};
     font-family: 'Inter', sans-serif;
   }
+
+  button {
+      margin-right: 11px;
+      border: 0;
+      color: ${props => props.theme.colors.tilte};
+      cursor: pointer;
+  }
+
+  input {
+    width: 100%;
+    height: 50px;
+    padding: 14px 16px 0 10px;
+    outline: 0;
+    border: 0;
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.subTilte};
+    border-bottom: 2px solid #BBB8D9;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-transform: capitalize;
+    padding: 0 50px;
+}
 `
