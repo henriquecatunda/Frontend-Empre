cc04e3fceab8204164b0d63a391e7e43c78aefc0
henrique.catunda <henriquecatunda2@gmail.com>
2021-11-29 23:37:54 -0300

    feat:repositório base

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main>
+       <h1>henrique</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
---
+++ src/styles/theme.tsx
@@ -0,0 +1,11 @@
+const theme = {
+    colors: {
+      background: '#F8F7FC',
+      text: '#6D6C7B',
+      tilte:'#4F46BB',
+      subTilte:'#302E45', 
+      primary: '#FFFFFF'
+    }
+  }
+  
+export default theme
